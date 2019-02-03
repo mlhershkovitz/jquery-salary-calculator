dfd487d1c838d3a6cf1cc4379e54fac37b47ae15
Madison Hershkovitz <mlhersh6@gmail.com>
2019-02-03 15:50:20 -0600

    append employees and debugged delete button

--- scripts/client.js
+++ scripts/client.js
@@ -4,19 +4,20 @@ $(document).ready(readyNow);
 let employeeArray = [];
 
 //employee class to help create new objects easily
-let Employee = {
-    constructor (firstName, lastName, idNumber, jobTitle, annualSalary){
-        this.firstName = firstName,
-        this.lastName = lastName,
-        this.idNumber = idNumber,
-        this.jobTitle = jobTitle,
-        this.annualSalary = annualSalary
+class Employee {
+    constructor(firstName, lastName, idNumber, jobTitle, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.jobTitle = jobTitle;
+        this.annualSalary = annualSalary;
     }
 }; //end employee class
 
 
 function readyNow() {
     $('#newEmployeeButton').on('click', addEmployee);
+    $('.employees').on('click', '#deleteButton', deleteEmployee);
 }; //end ready now
 
 //create a new employee when add is clicked
@@ -27,20 +28,45 @@ function addEmployee() {
     let idNumberIn = $('#idNumberIn').val();
     let jobTitleIn = $('#jobTitleIn').val();
     let annualSalaryIn = $('#annualSalaryIn').val();
-    let newEmployee = new Employee (firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn);
+    let newEmployee = new Employee(firstNameIn, lastNameIn, idNumberIn, jobTitleIn, annualSalaryIn);
     employeeArray.push(newEmployee);
 
+    //call empty input function
+    emptyInput();
+    //call function to append
+    appendEmployees();
+}; //end add employee 
+
+function emptyInput() {
     //empty inputs
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
     $('#idNumberIn').val('');
     $('#jobTitleIn').val('');
     $('#annualSalaryIn').val('');
-}; //end add employee 
-
+}; //end empty inputs
 //append new employees to dom when add clicked
 function appendEmployees() {
-    
+    let tableInfo = $('.employees');
+    tableInfo.empty();
+
+    //loop through array to append all info
+    for (let employee of employeeArray) {
+        tableInfo.append(`<tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.idNumber}</td>
+        <td>${employee.jobTitle}</td>
+        <td>${employee.annualSalary}</td>
+        <td><button id="deleteButton">Delete</button></td>
+        </tr>`);
+    }; //end loop
 }; //end append employees function
 
 //monthly total cost function
+
+//delete employee function
+function deleteEmployee() {
+    console.log('delete button clicked');
+
+}
